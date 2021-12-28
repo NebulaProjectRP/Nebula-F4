613684a94419a6db909b568bff65bf8a6349dd12
Gonzalolog <gonzalolog@gmail.com>
2021-12-28 00:50:12 -0300

    Update f4menu.lua

--- lua/vgui/f4menu.lua
+++ lua/vgui/f4menu.lua
@@ -12,16 +12,24 @@ function PANEL:Init()
     self.Tabs:SetTall(48)
     self.Tabs:DockMargin(16, 16, 16, 16)
 
-    self.Body = vgui.Create("DPanel", self)
+    self.Body = vgui.Create("Panel", self)
     self.Body:Dock(FILL)
     self.Body:DockMargin(32, 16, 32, 32)
     self.Tabs:SetContent(self.Body)
 
-    self.Tabs:AddTab("Jobs", "DPanel")
-    self.Tabs:AddTab("Inventory", "DPanel")
+    self.Tabs:AddTab("Jobs", "nebula.f4.jobs")
+    self.Tabs:AddTab("Inventory", "nebula.f4.inventory")
     self.Tabs:AddTab("Shop", "DPanel")
     self.Tabs:AddTab("Mining", "DPanel")
 
+    self:SetAlpha(0)
+    self:AlphaTo(255, .5, 0)
+end
+
+function PANEL:Paint(w, h)
+    surface.SetDrawColor(0, 0, 0, 240)
+    surface.DrawRect(0, 0, w, h)
+    self:DrawBlur( 3, 5 )
 end
 
 vgui.Register("nebula.f4", PANEL, "DFrame")
